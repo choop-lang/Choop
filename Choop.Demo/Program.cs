10a236261ac6f62e00e972b3064cd9b979d32896
chooper100 <chooper100.scratch@gmail.com>
2017-04-16 18:04:54 +0100

    Add initial test

--- Choop.Demo/Program.cs
+++ Choop.Demo/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Choop.Compiler;
 
 namespace Choop.Demo
 {
@@ -10,6 +13,17 @@ namespace Choop.Demo
     {
         static void Main(string[] args)
         {
+            AntlrInputStream input = new AntlrInputStream("sprite 0hello{}");
+
+            ChoopLexer lexer = new ChoopLexer(input);
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            ChoopParser parser = new ChoopParser(tokens);
+
+            Console.WriteLine(parser.compilation_unit().ToStringTree(parser));
+
+            Console.ReadLine();
         }
     }
 }
