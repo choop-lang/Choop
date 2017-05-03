dfd2eda2d7d995adcc2b973c2ce4681758a299ff
chooper100 <chooper100.scratch@gmail.com>
2017-05-03 01:55:07 +0100

    Add DataType enumeration

---
+++ Choop.Compiler/ChoopModel/DataType.cs
@@ -0,0 +1,25 @@
+﻿namespace Choop.Compiler.ChoopModel
+{
+    /// <summary>
+    /// Specifies the possible data types.
+    /// </summary>
+    enum DataType
+    {
+        /// <summary>
+        /// Indicates an object type.
+        /// </summary>
+        Object,
+        /// <summary>
+        /// Indicates the string type.
+        /// </summary>
+        String,
+        /// <summary>
+        /// Indicates the number type.
+        /// </summary>
+        Number,
+        /// <summary>
+        /// Indicates the boolean type.
+        /// </summary>
+        Boolean
+    }
+}
