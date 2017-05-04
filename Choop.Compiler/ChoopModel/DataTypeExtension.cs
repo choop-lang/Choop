a6e3c597c661582a771fe25d05d26a66ffd489e6
chooper100 <chooper100.scratch@gmail.com>
2017-05-04 01:06:05 +0100

    Add DataTypeExtension

---
+++ Choop.Compiler/ChoopModel/DataTypeExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Choop.Compiler.ChoopModel
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="DataType"/> enumeration. 
+    /// </summary>
+    static class DataTypeExtension
+    {
+        /// <summary>
+        /// Returns whether the specified data type is able to be stored within the current data type.
+        /// </summary>
+        /// <param name="type">The current data type.</param>
+        /// <param name="other">The data type to store.</param>
+        /// <returns>Whether the specified data type is able to be stored within the current data type.</returns>
+        public static bool IsCompatible(this DataType type, DataType other)
+        {
+            return type == DataType.Object || type == other;
+        }
+    }
+}
