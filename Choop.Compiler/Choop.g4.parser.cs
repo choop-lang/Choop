8c022f1b8f8fe64ffc76df42ba44ac4e900ea12c
chooper100 <chooper100.scratch@gmail.com>
2017-06-27 22:56:08 +0100

    Declare event names in parser

--- Choop.Compiler/Choop.g4.parser.cs
+++ Choop.Compiler/Choop.g4.parser.cs
@@ -1,10 +1,29 @@
-﻿using Antlr4.Runtime;
+﻿using System.Collections.Generic;
+using Antlr4.Runtime;
 using System.Collections.ObjectModel;
 
 namespace Choop.Compiler
 {
     sealed partial class ChoopParser
     {
+        #region Fields
+        /// <summary>
+        /// Gets the list of event names and their corresponding opcodes.
+        /// </summary>
+        public static readonly IDictionary<string, string> EventNames
+            = new Dictionary<string, string>
+            {
+                {"GreenFlag", "whenGreenFlag"},
+                {"KeyPressed", "whenKeyPressed"},
+                {"Clicked", "whenClicked"},
+                {"BackdropChanged", "whenSceneStarts"},
+                {"MessageRecieved", "whenIReceive"},
+                {"Cloned", "whenCloned"},
+                {"LoudnessGreaterThan", "whenSensorGreaterThan"}, // TODO: handling of hidden parameter?
+                {"TimerGreaterThan", "whenSensorGreaterThan"},
+                {"VideoMotionGreaterThan", "whenSensorGreaterThan"},
+            };
+        #endregion
         #region Constructor
         /// <summary>
         /// Creates a new instance of the <see cref="ChoopParser"/> class. 
