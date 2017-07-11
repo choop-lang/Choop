1ffb0db5be0fbce2d219ccdfe38c63e3d7bba0ea
chooper100 <chooper100.scratch@gmail.com>
2017-07-11 23:12:26 +0100

    Create IRule

---
+++ Choop.Compiler/ChoopModel/IRule.cs
@@ -0,0 +1,24 @@
+﻿using Antlr4.Runtime;
+
+namespace Choop.Compiler.ChoopModel
+{
+    /// <summary>
+    /// Represents an internal representation of a grammar rule.
+    /// </summary>
+    public interface IRule
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the token to mark any syntax errors detected during translation at.
+        /// </summary>
+        IToken ErrorToken { get; }
+
+        /// <summary>
+        /// Gets the file name where the rule was specified.
+        /// </summary>
+        string FileName { get; }
+
+        #endregion
+    }
+}
