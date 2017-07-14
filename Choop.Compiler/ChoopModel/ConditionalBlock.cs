95c3281ef9075a180f2e9c321670c2e8d1ff515b
chooper100 <chooper100.scratch@gmail.com>
2017-07-14 22:10:47 +0100

    Add ICompilable to ConditionalBlock

--- Choop.Compiler/ChoopModel/ConditionalBlock.cs
+++ Choop.Compiler/ChoopModel/ConditionalBlock.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Choop.Compiler.BlockModel;
+using Choop.Compiler.ObjectModel;
 
 namespace Choop.Compiler.ChoopModel
 {
     /// <summary>
     /// Represents a declaration of a block that is ran dependent on a condition.
     /// </summary>
-    public class ConditionalBlock : IHasBody
+    public class ConditionalBlock : IHasBody, ICompilable<Block[]>
     {
         #region Properties
 
@@ -23,6 +26,28 @@ namespace Choop.Compiler.ChoopModel
         /// Gets the collection of statements within the block.
         /// </summary>
         public Collection<IStatement> Statements { get; } = new Collection<IStatement>();
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Translates the Choop grammar structure.
+        /// </summary>
+        /// <param name="context">The context of the translation.</param>
+        /// <returns>The translated code for the grammar structure.</returns>
+        public Block[] Translate(TranslationContext context)
+        {
+            Scope newScope = new Scope(context.CurrentScope);
+            TranslationContext newContext = new TranslationContext(newScope, context.ErrorList);
+
+            List<Block> blocks = new List<Block>();
+
+            foreach (IStatement statement in Statements)
+                blocks.AddRange(statement.Translate(newContext));
+
+            return blocks.ToArray();
+        }
 
         #endregion
     }
