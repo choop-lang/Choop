a4683c1c2c4528813e361bde4df54f36cbec102d
chooper100 <chooper100.scratch@gmail.com>
2017-07-15 20:29:01 +0100

    Add IJsonConvertable

---
+++ Choop.Compiler/BlockModel/IJsonConvertable.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Choop.Compiler.BlockModel
+{
+    /// <summary>
+    /// Represents an object that can be serialized to JSON.
+    /// </summary>
+    public interface IJsonConvertable
+    {
+        #region Methods
+
+        /// <summary>
+        /// Serializes the current instance into a JSON object.
+        /// </summary>
+        /// <returns>The JSON representation of the current instance.</returns>
+        JToken ToJson();
+
+        #endregion
+    }
+}
