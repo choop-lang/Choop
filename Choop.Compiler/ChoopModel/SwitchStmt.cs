56867665c2264014d54647cb0f135740e1bebd5b
chooper100 <chooper100.scratch@gmail.com>
2017-07-22 01:20:26 +0100

    Fix Switch statement comparisons

--- Choop.Compiler/ChoopModel/SwitchStmt.cs
+++ Choop.Compiler/ChoopModel/SwitchStmt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Antlr4.Runtime;
 using Choop.Compiler.BlockModel;
 using Choop.Compiler.TranslationUtils;
@@ -61,16 +62,23 @@ namespace Choop.Compiler.ChoopModel
         /// <returns>The translated code for the grammar structure.</returns>
         public Block[] Translate(TranslationContext context)
         {
-            return BuildIfElse(context, 0);
+            // Create variable holder
+            StackValue variable = new StackValue("@expression", DataType.Object);
+            context.CurrentScope.StackValues.Add(variable);
+            Block[] declaration = variable.CreateDeclaration(Variable.Translate(context));
+
+            // Translate main switch
+            return declaration.Concat(BuildIfElse(context, new LookupExpression(variable, FileName, ErrorToken), 0)).ToArray();
         }
 
         /// <summary>
         /// Recursively builds the tranlsated if-else statement.
         /// </summary>
         /// <param name="context">The context of the translation.</param>
+        /// <param name="variable">The translated variable to compare to.</param>
         /// <param name="element">The current block.</param>
         /// <returns>The translated if-else statement.</returns>
-        private Block[] BuildIfElse(TranslationContext context, int element)
+        private Block[] BuildIfElse(TranslationContext context, IExpression variable, int element)
         {
             if (element == Blocks.Count)
                 throw new IndexOutOfRangeException("Element is out of range");
@@ -80,9 +88,9 @@ namespace Choop.Compiler.ChoopModel
                 {
                     new Block(
                         BlockSpecs.IfThenElse,
-                        BuildCondition(Blocks[element].Conditions).Translate(context),
+                        BuildCondition(Blocks[element].Conditions, variable).Translate(context),
                         Blocks[element].Translate(context),
-                        BuildIfElse(context, element + 1)
+                        BuildIfElse(context, variable, element + 1)
                     )
                 };
 
@@ -93,7 +101,7 @@ namespace Choop.Compiler.ChoopModel
             {
                 new Block(
                     BlockSpecs.IfThen,
-                    BuildCondition(Blocks[element].Conditions).Translate(context),
+                    BuildCondition(Blocks[element].Conditions, variable).Translate(context),
                     Blocks[element].Translate(context)
                 )
             };
@@ -103,13 +111,15 @@ namespace Choop.Compiler.ChoopModel
         /// Recursively builds the condition for a case block.
         /// </summary>
         /// <param name="conditions">The collection of input conditions.</param>
+        /// <param name="variable">The translated variable to compare to.</param>
         /// <param name="index">The current index.</param>
         /// <returns>The expression for the condition combining all input conditions.</returns>
-        private IExpression BuildCondition(Collection<IExpression> conditions, int index = 0) =>
+        private IExpression BuildCondition(Collection<IExpression> conditions, IExpression variable, int index = 0) =>
             index == conditions.Count - 1
-                ? conditions[index]
-                : new CompoundExpression(CompoundOperator.And, conditions[index],
-                    BuildCondition(conditions, index + 1), FileName, ErrorToken);
+                ? new CompoundExpression(CompoundOperator.Equal, variable, conditions[index], FileName, ErrorToken)
+                : new CompoundExpression(CompoundOperator.And,
+                    new CompoundExpression(CompoundOperator.Equal, variable, conditions[index], FileName, ErrorToken),
+                    BuildCondition(conditions, variable, index + 1), FileName, ErrorToken);
 
         #endregion
     }
