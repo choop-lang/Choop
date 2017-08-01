a8c4f69844e87ceadc5341d32a91df300423d406
chooper100 <chooper100.scratch@gmail.com>
2017-08-01 01:27:14 +0100

    Don't set return variable if no value

--- Choop.Compiler/ChoopModel/ReturnStmt.cs
+++ Choop.Compiler/ChoopModel/ReturnStmt.cs
@@ -54,6 +54,9 @@ namespace Choop.Compiler.ChoopModel
         /// <returns>The translated code for the grammar structure.</returns>
         public Block[] Translate(TranslationContext context)
         {
+            if (Value == null)
+                return new[] {new Block(BlockSpecs.Stop, "this script")};
+
             return new[]
             {
                 new Block(BlockSpecs.SetVariableTo, ((MethodDeclaration)context.CurrentScope.Method).GetReturnVariableName(), Value.Translate(context)),
