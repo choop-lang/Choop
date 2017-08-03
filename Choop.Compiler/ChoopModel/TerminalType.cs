ebcd4d7363225f108f9751c855671032c47c6c3d
chooper100 <chooper100.scratch@gmail.com>
2017-08-04 00:04:07 +0100

    Add TerminalType

---
+++ Choop.Compiler/ChoopModel/TerminalType.cs
@@ -0,0 +1,38 @@
+﻿namespace Choop.Compiler.ChoopModel
+{
+    /// <summary>
+    /// Specifies the possible terminal value formats.
+    /// </summary>
+    public enum TerminalType
+    {
+        /// <summary>
+        /// Indicates a boolean value.
+        /// </summary>
+        Bool,
+
+        /// <summary>
+        /// Indicates an escaped string value.
+        /// </summary>
+        String,
+
+        /// <summary>
+        /// Indicates a hexadecimal value.
+        /// </summary>
+        Hex,
+
+        /// <summary>
+        /// Indicates a value written in scientific notation.
+        /// </summary>
+        Scientific,
+
+        /// <summary>
+        /// Indicates a decimal value.
+        /// </summary>
+        Decimal,
+
+        /// <summary>
+        /// Indicates an integer value.
+        /// </summary>
+        Int
+    }
+}
