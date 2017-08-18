a667df8459c43876f3a307295c554258354ac5c7
chooper100 <chooper100.scratch@gmail.com>
2017-08-18 02:49:46 +0100

    Create DiskFileProvider

---
+++ Choop.Compiler/Interfaces/DiskFileProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Choop.Compiler.Interfaces
+{
+    /// <summary>
+    /// Provides functionality to read files from a disk within a Choop project.
+    /// </summary>
+    public class DiskFileProvider : FileProvider
+    {
+        #region Methods
+
+        /// <summary>
+        /// Returns the stream to read the file at the specified path, relative to the project base path.
+        /// </summary>
+        /// <param name="path">The path of the file being read, relative to the project base path.</param>
+        /// <returns>The stream to read the file from.</returns>
+        public override StreamReader GetFileReadStream(string path) => new StreamReader(ProjectPath + path);
+
+        #endregion
+    }
+}
