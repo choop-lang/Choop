70736cfa5c159d8c5357c9b78d63b34d1450ef7f
chooper100 <chooper100.scratch@gmail.com>
2017-08-18 02:41:20 +0100

    Create IFileProvider and FileProvider

---
+++ Choop.Compiler/Interfaces/FileProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+
+namespace Choop.Compiler.Interfaces
+{
+    /// <summary>
+    /// Provides base functionality to read files within a Choop project.
+    /// </summary>
+    public abstract class FileProvider : IFileProvider
+    {
+        #region Fields
+
+        /// <summary>
+        /// Whether a project is currently open.
+        /// </summary>
+        protected bool ProjectOpen;
+
+        /// <summary>
+        /// The path of the current open project.
+        /// </summary>
+        protected string ProjectPath;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Opens the project at the specified path.
+        /// </summary>
+        /// <param name="path">The path of the project being opened.</param>
+        public virtual void OpenProject(string path)
+        {
+            ProjectPath = path;
+            ProjectOpen = true;
+        }
+
+        /// <summary>
+        /// Returns the stream to read the file at the specified path, relative to the project base path.
+        /// </summary>
+        /// <param name="path">The path of the file being read, relative to the project base path.</param>
+        /// <returns>The stream to read the file from.</returns>
+        public abstract StreamReader GetFileReadStream(string path);
+
+        /// <summary>
+        /// Closes the current open project.
+        /// </summary>
+        public virtual void CloseProject()
+        {
+            ProjectPath = string.Empty;
+            ProjectOpen = false;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public virtual void Dispose()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Choop.Compiler/Interfaces/IFileProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Choop.Compiler.Interfaces
+{
+    /// <summary>
+    /// Provides functionality to read files within a Choop project.
+    /// </summary>
+    public interface IFileProvider : IDisposable
+    {
+        #region Methods
+
+        /// <summary>
+        /// Opens the project at the specified path.
+        /// </summary>
+        /// <param name="path">The path of the project being opened.</param>
+        void OpenProject(string path);
+
+        /// <summary>
+        /// Returns the stream to read the file at the specified path, relative to the project base path.
+        /// </summary>
+        /// <param name="path">The path of the file being read, relative to the project base path.</param>
+        /// <returns>The stream to read the file from.</returns>
+        StreamReader GetFileReadStream(string path);
+
+        /// <summary>
+        /// Closes the current open project.
+        /// </summary>
+        void CloseProject();
+
+        #endregion
+    }
+}
