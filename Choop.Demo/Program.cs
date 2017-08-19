ecc78cd2a5d2f856750e1c44516acda1e60483dd
chooper100 <chooper100.scratch@gmail.com>
2017-08-19 20:56:16 +0100

    Use LoadProject in demo

--- Choop.Demo/Program.cs
+++ Choop.Demo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using Choop.Compiler;
 using Choop.Compiler.Interfaces;
 
@@ -13,42 +12,38 @@ namespace Choop.Demo
         /// <param name="args">The arguments supplied to the program.</param>
         private static void Main(string[] args)
         {
-            const string inputFile = @"Samples\Fibonacci\Fibonacci.chs";
+            const string inputProject = @"Samples\Fibonacci\";
             const string outputFile = @"Output\project.json";
 
-            // Open file
-            using (StreamReader reader = new StreamReader(inputFile))
+            // Create compiler instance
+            ChoopCompiler compiler = new ChoopCompiler(new DiskFileProvider());
+
+            // Compile code
+            compiler.LoadProject(inputProject);
+            compiler.Compile();
+
+            // Check if compilation was successful
+            if (compiler.HasErrors)
             {
-                // Create compiler instance
-                ChoopCompiler compiler = new ChoopCompiler(new DiskFileProvider());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not compile file:");
 
-                // Compile code
-                compiler.InjectCode(reader.BaseStream, inputFile);
-                compiler.Compile();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                foreach (CompilerError error in compiler.CompilerErrors)
+                    Console.WriteLine(string.IsNullOrEmpty(error.TokenText)
+                        ? $"Line {error.Line}:{error.Col}  {error.Message} ({error.FileName})"
+                        : $"Line {error.Line}:{error.Col}..{error.StopIndex - error.StartIndex + error.Col}\t{error.Message} ({error.FileName})");
+            }
+            else
+            {
+                // Compilation finished
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Successfully compiled!\r\n");
 
-                // Check if compilation was successful
-                if (compiler.HasErrors)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Could not compile file:");
+                compiler.Save(outputFile);
 
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    foreach (CompilerError error in compiler.CompilerErrors)
-                        Console.WriteLine(string.IsNullOrEmpty(error.TokenText)
-                            ? $"Line {error.Line}:{error.Col}  {error.Message} ({error.FileName})"
-                            : $"Line {error.Line}:{error.Col}..{error.StopIndex - error.StartIndex + error.Col}\t{error.Message} ({error.FileName})");
-                }
-                else
-                {
-                    // Compilation finished
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Successfully compiled!\r\n");
-
-                    compiler.Save(outputFile);
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Saved to {0}", outputFile);
-                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Saved to {0}", outputFile);
             }
 
             // Keep console open until dismissed by user
