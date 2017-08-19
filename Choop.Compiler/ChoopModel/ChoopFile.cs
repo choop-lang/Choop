8f56ec5e1434c306b1d746fb4ce37fa77ed1cdea
chooper100 <chooper100.scratch@gmail.com>
2017-08-19 18:15:14 +0100

    Add ChoopFile serialisation attributes

--- Choop.Compiler/ChoopModel/ChoopFile.cs
+++ Choop.Compiler/ChoopModel/ChoopFile.cs
@@ -1,8 +1,11 @@
-﻿namespace Choop.Compiler.ChoopModel
+﻿using Newtonsoft.Json;
+
+namespace Choop.Compiler.ChoopModel
 {
     /// <summary>
     /// Represents a file inside a Choop project.
     /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
     public class ChoopFile
     {
         #region Properties
@@ -10,16 +13,19 @@
         /// <summary>
         /// Gets or sets the attribution for the file.
         /// </summary>
+        [JsonProperty("attribution")]
         public string Attribution { get; set; }
 
         /// <summary>
         /// Gets or sets the build action for the file.
         /// </summary>
+        [JsonProperty("buildAction")]
         public BuildAction BuildAction { get; set; }
 
         /// <summary>
         /// Gets or sets the path of the file, relative to the project base path.
         /// </summary>
+        [JsonProperty("path")]
         public string Path { get; set; }
 
         #endregion
