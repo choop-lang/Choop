cdb438b97097ec21a2410345f581ac6983be2fee
chooper100 <chooper100.scratch@gmail.com>
2017-08-20 18:24:20 +0100

    Add CostumeAsset

---
+++ Choop.Compiler/ProjectModel/CostumeAsset.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Choop.Compiler.ProjectModel
+{
+    /// <summary>
+    /// Represents a costume in a sprite.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    public class CostumeAsset
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the path to the image referenced.
+        /// </summary>
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the costume.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
