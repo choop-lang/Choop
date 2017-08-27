8a84d5a88916239ebf0ce4c2921472cbc553a483
chooper100 <chooper100.scratch@gmail.com>
2017-08-27 01:17:44 +0100

    Add Type to MethodSignature

--- Choop.Compiler/Helpers/MethodSignature.cs
+++ Choop.Compiler/Helpers/MethodSignature.cs
@@ -1,4 +1,6 @@
-﻿namespace Choop.Compiler.Helpers
+﻿using Choop.Compiler.ChoopModel;
+
+namespace Choop.Compiler.Helpers
 {
     /// <summary>
     /// Represents the signature for an inbuilt common Scratch block.
@@ -18,6 +20,11 @@
         public bool IsReporter { get; }
 
         /// <summary>
+        /// Gets the return type of the method.
+        /// </summary>
+        public DataType Type { get; }
+
+        /// <summary>
         /// The names of the inputs to the method, in order.
         /// </summary>
         public string[] Inputs { get; }
@@ -31,12 +38,14 @@
         /// </summary>
         /// <param name="name">The name of the method.</param>
         /// <param name="isReporter">Whether the method reports a value.</param>
+        /// <param name="type">The return type of the method.</param>
         /// <param name="inputs">The names of inputs to the method, in order.</param>
-        public MethodSignature(string name, bool isReporter, params string[] inputs)
+        public MethodSignature(string name, bool isReporter, DataType type, params string[] inputs)
         {
             Name = name;
             Inputs = inputs;
             IsReporter = isReporter;
+            Type = type;
         }
 
         #endregion
